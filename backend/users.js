2fb670a702befa5ed10a7c977ed5e2e2aaf707e3
Cláudio <claudio@techsupport.com>
2025-11-04 13:09:17 -0300

    chore: remove legacy JS/HTML files after TypeScript migration

--- backend/users.js
+++
@@ -1,8 +0,0 @@
-const users = [
-    {name: 'Jonathan', password: '1234', role: 'funcionario'},
-    {name: 'Luka', password: 'abcd', role: 'tecnico'},
-    {name: 'Camila', password: '5678', role: 'tecnico'},
-    {name: 'Augusto', password: 'fgeh', role: 'funcionario'}
-];
-
-module.exports = { users };
